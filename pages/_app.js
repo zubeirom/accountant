be3acfbf4e019fec50e9546a141b66ca109744c4
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2021-12-29 22:37:12 +0100

    setup auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<SessionProvider session={pageProps.session}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</SessionProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
